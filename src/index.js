21618b13856b4f7db367341fe5a2b8fcb68d3508
jayparmar271 <jayparmar271@gmail.com>
2020-12-13 23:36:12 +0530

    Add authorization to get profile

--- src/index.js
+++ src/index.js
@@ -6,6 +6,18 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
+// app.use((req, res, next) => {
+//   if (req.method === 'GET') {
+//     res.send('Get requests are disabled')
+//   } else {
+//     next()
+//   }
+// })
+
+// app.use((req, res, next) => {
+//   res.status(503).send('Site is currently down. Check back soon!')
+// })
+
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
@@ -15,13 +27,3 @@ app.listen(port, () => {
 })
 
 const jwt = require('jsonwebtoken')
-
-const myFunction = async () => {
-  const token = jwt.sign({ _id: 'abc123' }, 'jwtsecretkey', { expiresIn: '7 days' })
-  console.log(token)
-
-  const data = jwt.verify(token, 'jwtsecretkey');
-  console.log(data)
-}
-
-myFunction()
